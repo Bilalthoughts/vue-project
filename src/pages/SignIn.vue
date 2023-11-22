b250d4374624c55d189b43c47d7e49a2f603446a
Bilalthoughts <80778230+Bilalthoughts@users.noreply.github.com>
2023-11-22 21:42:51 +0500

    bugfix

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -25,7 +25,7 @@
             name=""
             id="email"
             class="inputclass"
-            :class="{ 'border-danger': !v$.form.email.$pending && v$.form.email.$error }"
+            :class="{ 'border-danger': !v$.form.email.$pending && v$.form.email.$error && buttonClick }"
           />
           <div class="text-danger" v-for="(error, index) of v$.form.email.$errors" :key="index">
         <div  v-if="this.buttonClick " class="error-msg"><small>{{error.$message}}</small></div>
@@ -47,7 +47,7 @@
           id="password"
           class="inputclass"
           @blur="v$.form.password.$touch"
-          :class="{ 'border-danger': !v$.form.password.$pending && v$.form.password.$error }"
+          :class="{ 'border-danger': !v$.form.password.$pending && v$.form.password.$error && buttonClick }"
           
         />
         <div  class="text-danger" v-for="(error, index) of v$.form.password.$errors" :key="index">
