a733b789f3420700697108b28f912c95395702e0
M. Faran Muneer <faran.muneer@teknitt.com>
2023-12-01 01:16:03 +0500

    json images

---
+++ src/data/po-inprogress.js
@@ -0,0 +1,17 @@
+export const po_inprogress_list = {
+  total_count: 1,
+  data: [
+    {
+      POA: 'Sent',
+      Ship: '',
+      INV: '',
+      id: 115,
+      PO_Number: '2957609',
+      PO_Type: 'New Order',
+      Trading_Partner: 'Stryker',
+      Date_Posted: '2023-06-21 23:17:36.664130+00:00',
+      PO_Date: '09/02/2020',
+      PO_Status: 'In Progress',
+    },
+  ],
+};
