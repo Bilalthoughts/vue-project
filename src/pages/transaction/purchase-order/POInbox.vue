af9ad02efca754789a8a512fddaa58c1af9f5898
M. Faran Muneer <faran.muneer@teknitt.com>
2023-11-30 22:56:41 +0500

    fix

--- src/pages/transaction/purchase-order/POInbox.vue
+++ src/pages/transaction/purchase-order/POInbox.vue
@@ -11,44 +11,32 @@
         <span class="d-flex gap-2">
           Show
           <div class="dropdown">
-            <span
-              class="dropdown-toggle"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
+            <span class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               {{ noOfRows }}
             </span>
             Entries
             <ul class="dropdown-menu">
               <li>
-                <a class="dropdown-item" @click="selectedValue(10)">10</a>
+                <a class="dropdown-item" @click="noOfRows = 10">10</a>
               </li>
               <li>
-                <a class="dropdown-item" @click="selectedValue(20)">20</a>
+                <a class="dropdown-item" @click="noOfRows = 20">20</a>
               </li>
               <li>
-                <a class="dropdown-item" @click="selectedValue(30)">30</a>
+                <a class="dropdown-item" @click="noOfRows = 30">30</a>
               </li>
               <li>
-                <a class="dropdown-item" @click="selectedValue(40)">40</a>
+                <a class="dropdown-item" @click="noOfRows = 40">40</a>
               </li>
             </ul>
           </div></span
         >
         <span
           >Search
-          <input
-            v-model="search"
-            class="border border-dark rounded"
-            type="text"
-          />
+          <input v-model="search" class="border border-dark rounded" type="text" />
 
           <span class="d-block text-end"
-            ><span @click="filterByPoStatus('')" class="shadow btn"
-              ><img src="../../../assets/icons/refresh.svg" alt="" /> Clear All
-              Filter</span
-            ></span
+            ><span @click="clearFilter()" class="shadow btn"><img src="../../../assets/icons/refresh.svg" alt="" /> Clear All Filter</span></span
           >
         </span>
       </div>
@@ -58,79 +46,41 @@
             <tr class="bg-light">
               <th scope="col">PO NUMBER</th>
               <th scope="col">PO TYPE</th>
-              <th scope="col">TRADING PARTNER ID</th>
-              <th scope="col">DATE POSTED</th>
-              <th scope="col">PO DATE</th>
               <th scope="col">
                 <div class="dropdown d-inline">
-                  <span
-                    class="dropdown-toggle removeArrow"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
+                  <span class="dropdown-toggle removeArrow" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="../../../assets/icons/filter.svg" alt="" />
                   </span>
 
                   <ul class="dropdown-menu">
-                    <li v-for="items in poStatusDataArray" :key="items">
-                      <a
-                        class="dropdown-item"
-                        @click="filterByPoStatus(items)"
-                        >{{ items }}</a
-                      >
+                    <li v-for="item in allUniquePOStatus" :key="item">
+                      <a class="dropdown-item" @click="statusSelected = item">{{ item }}</a>
+                    </li>
+                  </ul>
+                </div>
+                TRADING PARTNER ID</th>
+              <th scope="col">DATE POSTED</th>
+              <th scope="col">PO DATE</th>
+              <th scope="col">
+                <div class="dropdown d-inline">
+                  <span class="dropdown-toggle removeArrow" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="../../../assets/icons/filter.svg" alt="" />
+                  </span>
+
+                  <ul class="dropdown-menu">
+                    <li v-for="item in allUniquePOStatus" :key="item">
+                      <a class="dropdown-item" @click="statusSelected = item">{{ item }}</a>
                     </li>
                   </ul>
                 </div>
                 PO STATUS
-                <img
-                  v-if="isArrow"
-                  @click="isArrowMethode"
-                  src="../../../assets/icons/arrowDown.svg"
-                  alt=""
-                />
-                <img
-                  v-if="isArrow === false"
-                  @click="isArrowMethode"
-                  src="../../../assets/icons/arrowUp.svg"
-                  alt=""
-                />
+                <img @click="statusSort = !statusSort" :src="statusSort ? require('../../../assets/icons/arrowDown.svg') : require('../../../assets/icons/arrowUp.svg')" alt="" />
               </th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="items in filteredArray" :key="items.id">
               <td>{{ items.PO_Number }}</td>
-              <!-- <td>
-                <div class="dropdown border w-25">
-                  <span
-                    class="dropdown-toggle"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      class="bi bi-list"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                      />
-                    </svg>
-                  </span>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">20</a></li>
-                    <li><a class="dropdown-item" href="#">30</a></li>
-                    <li><a class="dropdown-item" href="#">40</a></li>
-                  </ul>
-                </div>
-              </td> -->
               <td>{{ items.PO_Type }}</td>
               <td>{{ items.Trading_Partner }}</td>
               <td>{{ items.Date_Posted }}</td>
@@ -152,7 +102,7 @@
             v-model="page"
             :records="po_inbox_list.total_count"
             :per-page="noOfRows"
-            @paginate="myCallback"
+            @paginate="myCallback()"
             :options="{
               chunk: 3,
             }"
@@ -169,85 +119,75 @@
 </style>
 
 <script>
-import { po_inbox_list } from "../../../data/po-inbox.js";
+import { po_inbox_list } from '../../../data/po-inbox.js';
 export default {
-  name: "DashboardComp",
+  name: 'DashboardComp',
   data() {
     return {
       po_inbox_list: po_inbox_list,
-      search: "",
       page: 1,
       noOfRows: 10,
-      filteredData: [],
-      poStatusvalue: "",
-      poStatusDataArray: new Set(),
-      isArrow: false,
+      search: '',
+      statusSelected: '',
+      statusSort: false,
+      tPSelected: '',
+      tPSort: false,
     };
   },
   methods: {
-    selectedValue(value) {
-      return (this.noOfRows = value);
+    clearFilter() {
+      this.statusSelected = '';
+      this.tPSelected = '';
     },
-    filterByPoStatus(status) {
-      this.poStatusvalue = status.toLowerCase();
-      console.log(this.filteredData, "this.filteredData ");
-      if (this.poStatusvalue) {
-        this.filteredData = this.po_inbox_list.data.filter(
-          (item) =>
-            item.PO_Status.toLowerCase() === this.poStatusvalue.toLowerCase()
-        );
-      }
+    myCallback() {
+      // console.log();
     },
-
-    isArrowMethode() {
-      return (this.isArrow = !this.isArrow);
-    },
+    // sortData(arrFiltered) {
+    //   if (this.statusSort) {
+    //     arrFiltered = arrFiltered.sort((a, b) => {
+    //       if (a.PO_Status < b.PO_Status) {
+    //         return -1;
+    //       }
+    //       if (a.PO_Status > b.PO_Status) {
+    //         return 1;
+    //       }
+    //       return 0;
+    //     });
+    //   } else {
+    //     arrFiltered = arrFiltered.sort((a, b) => {
+    //       if (a.PO_Status > b.PO_Status) {
+    //         return -1;
+    //       }
+    //       if (a.PO_Status < b.PO_Status) {
+    //         return 1;
+    //       }
+    //       return 0;
+    //     });
+    //   }
+    //   return arrFiltered;
+    // },
   },
   computed: {
-    // filteredArray() {
-    //   return this.poStatusvalue !== "" && this.filteredData.length > 0
-    //     ? this.filteredData
-    //     : this.po_inbox_list.data.filter((item) =>
-    //         Object.values(item).some((value) =>
-    //           String(value).toLowerCase().includes(this.search.toLowerCase())
-    //         )
-    //       );
-    // },
-
+    allUniquePOStatus() {
+      return [...new Set(this.po_inbox_list.data.map((item) => item.PO_Status))];
+    },
+    allUniqueTP() {
+      return [...new Set(this.po_inbox_list.data.map((item) => item.Trading_Partner))];
+    },
     filteredArray() {
-      if (this.poStatusvalue !== "") {
-        return this.filteredData;
-      } else if (this.search !== "") {
-        return this.po_inbox_list.data.filter((item) =>
-          Object.values(item).some((value) =>
-            String(value).toLowerCase().includes(this.search.toLowerCase())
-          )
-        );
+      let arrFiltered = this.po_inbox_list.data;
+      if (this.search !== '') {
+        arrFiltered = arrFiltered.filter((item) => Object.values(item).some((value) => String(value).toLowerCase().includes(this.search.toLowerCase())));
       }
-      return this.po_inbox_list.data;
+      if (this.statusSelected !== '') {
+        arrFiltered = arrFiltered.filter((item) => item.PO_Status.toLowerCase() === this.statusSelected.toLowerCase());
+      }
+      if (this.tPSelected !== '') {
+        arrFiltered = arrFiltered.filter((item) => item.Trading_Partner.toLowerCase() === this.tPSelected.toLowerCase());
+      }
+      // arrFiltered = this.sortData(arrFiltered);
+      return arrFiltered;
     },
-    poStatusArray() {
-      return this.poStatusDataArray;
-    },
-    sortedArrayAlphabetically() {
-      let arrayCopy = [...this.filteredArray];
-      return arrayCopy.sort((a, b) => {
-        let fa = a.PO_Status.toLowerCase();
-        let fb = b.PO_Status.toLowerCase();
-        if (this.isArrow && fa < fb) {
-          return -1;
-        }
-        if (this.isArrow === false && fa > fb) {
-          return 1;
-        }
-        return 0;
-      });
-    },
-  },
-  created() {
-    this.poStatusDataArray = new Set(
-      this.po_inbox_list.data.map((item) => item.PO_Status)
-    );
   },
 };
 </script>
